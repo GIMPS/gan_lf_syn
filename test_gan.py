86d1a74e7d70e855094aaf2b6f053b8cfa87d66c
Jason Ren <renjiawei1997@gmail.com>
2018-06-30 12:20:40 +0800

    minor fix

--- test_gan.py
+++ test_gan.py
@@ -72,7 +72,7 @@ def synthesize_novel_views(depth_net, color_net, inputLF, fullLF, resultPath):
         # write the numerical evaluation and the final image
         if indY == 4 and indX == 4:
             write_error(curEst, curRef, resultPath)
-        img = curRef.cpu().numpy()
+        img = curEst.cpu().numpy()
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         imwrite(resultPath + '/Images_GAN/' + ('%02d_%02d.png' % (indY, indX)), (adjust_tone(img) * 255).astype(int))
 
